76718c81c2a9135d44ca05ed6f59a7aaf13bbf7b
Zoe Günther <zozogue7@gmail.com>
2022-05-17 20:24:19 +0200

    made few bugfixes and changes to mongodb

--- backend/api.js
+++ backend/api.js
@@ -1,98 +1,67 @@
-const TODOS = [
-    {
-        id: 0,
-        title: 'Aufgabe 4 abgeben',
-        due: '2021-05-21T10:00:00.000Z',
-        status: 'done'
-    },
-    {
-        id: 1,
-        title: 'Aufgabe 6 abgeben',
-        due: '2021-06-08T10:00:00.000Z',
-        status: 'doing'
-    },
-    {
-        id: 2,
-        title: 'ToDo-Anwendung fertig stellen',
-        due: '2021-06-22T10:00:00.000Z',
-        status: 'open'
-    },
-    {
-        id: 3,
-        title: 'Für die Klausur lernen',
-        due: '2021-07-01T11:00:00.000Z',
-        status: 'open'
-    }
-];
+let db = require('./persistence/mongodb.js');
 
-function getFreeTodoID() {
-    let index = 0;
-
-    while(TODOS.find(element => element.id === index)) {
-        index++;
-    }
-
-    return index;
+exports.init = () => {
+    return db.connect();
 }
 
-
-
 exports.getTodos = (req, res) => {
-    console.log(`getTodo: ${JSON.stringify(req.body)}`);
-    res.send(TODOS);
+
+    console.log("getTodo: %o", req.body);
+    db.queryAll()
+        .then(result => {
+            console.log("getTodo: db returned %o", result);
+            res.send(result);
+        })
+        .catch(err => {
+            console.log("error: %o", err);
+            res.status(500).send(err)
+        });
+
 }
 
 exports.createTodo = (req, res) => {
-    console.log(`createTodo: ${JSON.stringify(req.body)}`);
 
-    var new_todo = req.body;
-    new_todo.id = getFreeTodoID();
-    new_todo.status = 'open';
+    console.log("createTodo: %o", req.body);
+    db.insert(req.body)
+        .then(result => {
+            console.log("createTodo: db returned %o", result);
+            
+            db.queryById(result.insertedId)
+                .then(re => {
+                    res.send(re);
+                })
+                .catch(err => res.status(500).send(err));
+        })
+        .catch(err => res.status(500).send(err));
 
-    TODOS.push(new_todo);
+}
 
-    res.send(new_todo);
-},
-
+/** TODO: FIX UPDATE ERROR */
 exports.updateTodo = (req, res) => {
     let id = req.params.id;
 
-    const element = TODOS.find(element => element.id == id);
-
-    if(element == undefined) {
-        res.status(400);
-        res.send("Ungültige ID");
-        return;
-    }
-
-    console.log(`updateTodo: ${id} ${JSON.stringify(req.body)}`);
-    let updated_todo = req.body;
-
-    const elementIndex = TODOS.findIndex(element => element.id == id);
-    for(var i in updated_todo) {
-        TODOS[elementIndex][i] = updated_todo[i];
-    }
-    
-    res.send(TODOS[elementIndex]);
-},
+    console.log("updateTodo: %s %o", id, req.body);
+    db.update(id, req.body)
+        .then(result => {
+            console.log("updateTodo: db returned %o", result);
+            
+            db.queryById(id)
+                .then(re => {
+                    res.send(re);
+                })
+                .catch(err => res.status(500).send(err));
+        })
+        .catch(err => res.status(500).send(err));
+}
 
 exports.deleteTodo = (req, res) => {
     let id = req.params.id;
 
-    const element = TODOS.find(element => element.id == id);
-
-    if(element == undefined) {
-        res.status(400);
-        res.send("Ungültige ID");
-        return;
-    }
-
-    console.log(`deleteTodo: ${id}`);
-
-    const elementIndex = TODOS.findIndex(element => element.id == id);
-
-    TODOS.splice(elementIndex, 1);
-
-    res.status(204);
-    res.send();
+    console.log("deleteTodo: %s", id);
+    db.delete(id)
+        .then(result => {
+            console.log("deleteTodo: db returned %o", result);
+            res.status(204).send();
+        })
+        .catch(err => res.status(500).send(err));
 }
--- backend/app.js
+++ backend/app.js
@@ -10,19 +10,23 @@ app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-app.route('/api/todos')
+api.init().then(() => {
+
+    app.route('/api/todos')
     .get(api.getTodos)
     .post(api.createTodo);
 
-app.route('/api/todos/:id')
-    .put(api.updateTodo)
-    .delete(api.deleteTodo);
+    app.route('/api/todos/:id')
+        .put(api.updateTodo)
+        .delete(api.deleteTodo);
 
-app.listen(port, (error) => {
-    if(error) {
-        console.log(`Error occured while starting to listen on Port ${port}`, error);
-        return;
-    }
+    app.listen(port, (error) => {
+        if(error) {
+            console.log(`Error occured while starting to listen on Port ${port}`, error);
+            return;
+        }
 
-    console.log(`App is listening on port ${port}`);
-});
+        console.log(`App is listening on port ${port}`);
+    });
+
+}).catch((err) => console.error("initialization error: %o", err));
